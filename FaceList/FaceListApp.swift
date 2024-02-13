1ba48c99c860c8d7aca3d27a800799b02e5ed752
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2024-02-13 11:52:20 +0200

    Initial Commit

---
+++ FaceList/FaceListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceListApp.swift
+//  FaceList
+//
+//  Created by Oleg Gavashi on 13.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct FaceListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
